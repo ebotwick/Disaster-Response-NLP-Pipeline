34c8b816f3b977395fefe6752b31e663814d8a43
Elliott Botwick <elliott.botwick@dominodatalab.com>
2021-05-13 13:08:30 -0700

    Update app.sh
    Pushed from Domino: https://demo.dominodatalab.com/u/elliott_botwick/NLP-Disaster-Response/runs/609d31fe23ba39386499b00d

--- .ipynb_checkpoints/app-checkpoint.sh
+++ .ipynb_checkpoints/app-checkpoint.sh
@@ -3,6 +3,10 @@
 # This is a bash script for Domino's App publishing feature
 # Learn more at http://support.dominodatalab.com/hc/en-us/articles/209150326
  
+ 
+## First install necessary packages not already in environment
+pip install plotly
+
 ## Flask example
 ## This is an example of the code you would need in this bash script for a Python/Flask app
 #export LC_ALL=C.UTF-8
